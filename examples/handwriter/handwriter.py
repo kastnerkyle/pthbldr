f6681c3147deefdbeefc3f7ad8fb56186947b6d5
Kyle Kastner <kastnerkyle@gmail.com>
2017-11-29 23:26:51 -0500

    Converted attention to cell...

--- examples/handwriter/handwriter.py
+++ examples/handwriter/handwriter.py
@@ -5,6 +5,7 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch as th
 import torch.nn.functional as F
+from torch.nn.modules.module import _addindent
 
 import numpy as np
 import time
@@ -28,9 +29,9 @@ pen_trace = np.array([x.astype(floatX) for x in X])
 chars = np.array([yy.astype(floatX) for yy in y])
 
 minibatch_size = 50
-n_epochs = 3  # Used way at the bottom in the training loop!
+n_epochs = 100  # Used way at the bottom in the training loop!
 cut_len = 300  # Used way at the bottom in the training loop!
-learning_rate = 1E-5
+learning_rate = 1E-4
 random_state = np.random.RandomState(1999)
 
 train_itr = list_iterator([pen_trace, chars], minibatch_size, axis=1, stop_index=10000,
@@ -54,6 +55,36 @@ use_cuda = True
 # try to get deterministic runs
 th.manual_seed(1999)
 random_state = np.random.RandomState(1999)
+
+def torch_summarize(model, show_weights=True, show_parameters=True):
+    """Summarizes torch model by showing trainable parameters and weights."""
+    tmpstr = model.__class__.__name__ + ' (\n'
+    global_params = 0
+    for key, module in model._modules.items():
+        # if it contains layers let call it recursively to get params and weights
+        if type(module) in [
+            nn.modules.container.Container,
+            nn.modules.container.Sequential
+        ]:
+            modstr = torch_summarize(module)
+        else:
+            modstr = module.__repr__()
+        modstr = _addindent(modstr, 2)
+
+        params = sum([np.prod(p.size()) for p in module.parameters()])
+        weights = tuple([tuple(p.size()) for p in module.parameters()])
+
+        tmpstr += '  (' + key + '): ' + modstr
+        if show_weights:
+            tmpstr += ', weights={}'.format(weights)
+        if show_parameters:
+            tmpstr +=  ', parameters={}'.format(params)
+        tmpstr += '\n'
+        global_params += params
+    tmpstr = tmpstr + ')'
+    tmpstr += "\n"
+    tmpstr += "Total parameters={0:.4f} M".format(global_params / 1E6)
+    return tmpstr
 
 class GLinear(nn.Module):
     def __init__(self, input_size, output_size):
@@ -123,10 +154,11 @@ class GGRUCell(nn.Module):
         return h_i
 
 
-class GaussianAttention(nn.Module):
+# TODO: Change logic to cell based... oy
+class GaussianAttentionCell(nn.Module):
     def __init__(self, c_input_size, input_size, hidden_size, n_components, minibatch_size,
                  cell_type="GRU"):
-        super(GaussianAttention, self).__init__()
+        super(GaussianAttentionCell, self).__init__()
         self.c_input_size = c_input_size
         self.input_size = input_size
         self.n_components = n_components
@@ -161,56 +193,34 @@ class GaussianAttention(nn.Module):
         ss4 = ss3.sum(dim=1)[:, 0, :]
         return ss4
 
-    def forward(self, c_inp, inp, gru_init, att_init):
-        ts = inp.size(0)
+    def forward(self, c_inp, inp_t, gru_h_tm1, att_k_tm1):
         cts = c_inp.size(0)
-        minibatch_size = inp.size(1)
-        hiddens = Variable(th.zeros(ts, minibatch_size, self.input_size))
-        att_k = Variable(th.zeros(ts, minibatch_size, self.n_components))
-        att_w = Variable(th.zeros(ts, minibatch_size, self.input_size))
-        if use_cuda:
-            hiddens = hiddens.cuda()
-            att_k = att_k.cuda()
-            att_w = att_w.cuda()
-        #hiddens = []
-        #att_k = []
-        #att_w = []
+        minibatch_size = c_inp.size(1)
 
-        k_tm1 = att_init
-        h_tm1 = gru_init
+        k_tm1 = att_k_tm1
+        h_tm1 = gru_h_tm1
         # input needs to be projected to hidden size and merge with cell...
         # otherwise this is junk
 
         u = Variable(th.FloatTensor(th.arange(0, cts)))[None, None, :]
         if use_cuda:
             u = u.cuda()
+            k_tm1 = att_k_tm1.cuda()
+            h_tm1 = gru_h_tm1.cuda()
 
-        for i in range(ts):
-            inp_t = inp[i]
-            a_t = self.att_a(inp_t).exp_()
-            b_t = self.att_b(inp_t).exp_()
-            att_k_o = self.att_k(inp_t).exp_()
-            k_t = k_tm1.expand_as(att_k_o) + att_k_o
-            ss4 = self.calc_phi(k_t, a_t, b_t, u)
-            ss5 = ss4[:, :, None]
-            ss6 = ss5.expand(ss5.size(0), ss5.size(1), c_inp.size(2)) * c_inp.permute(1, 0, 2)
-            w_t = ss6.sum(dim=1)[:, 0, :]
+        a_t = self.att_a(inp_t).exp_()
+        b_t = self.att_b(inp_t).exp_()
+        att_k_o = self.att_k(inp_t).exp_()
+        k_t = k_tm1.expand_as(att_k_o) + att_k_o
+        ss4 = self.calc_phi(k_t, a_t, b_t, u)
+        ss5 = ss4[:, :, None]
+        ss6 = ss5.expand(ss5.size(0), ss5.size(1), c_inp.size(2)) * c_inp.permute(1, 0, 2)
+        w_t = ss6.sum(dim=1)[:, 0, :]
 
-            inp_t = self.inp_fork(inp_t)
-            f_t = self.fork(w_t + inp_t)
-            h_t = self.cell(f_t, h_tm1)
-
-            att_w[i] = att_w[i] + w_t
-            att_k[i] = att_k[i] + k_t
-            hiddens[i] = hiddens[i] + h_t
-
-            #att_w.append(w_t)
-            #att_k.append(k_t)
-            #hiddens.append(h_t)
-            h_tm1 = h_t
-            k_tm1 = k_t
-            w_tm1 = w_t
-        return hiddens, att_k, att_w
+        finp_t = self.inp_fork(inp_t)
+        f_t = self.fork(w_t + finp_t)
+        h_t = self.cell(f_t, h_tm1)
+        return h_t, k_t, w_t
 
     def create_inits(self):
         if self.cell_type == "GRU":
@@ -238,16 +248,39 @@ def softmax(inp, eps=1E-6, axis=1):
     soft_max_nd = soft_max_2d.view(*trans_size)
     return soft_max_nd.transpose(axis, len(input_size)-1)
 
+
 # https://discuss.pytorch.org/t/gradient-clipping/2836/14
 def clip_gradient(model, clip):
     """Clip the gradient"""
     if clip is None:
         return
-    totalnorm = 0
     for p in model.parameters():
         if p.grad is None:
             continue
         p.grad.data = p.grad.data.clamp(-clip, clip)
+
+
+def norm_gradient(model, rescale):
+    """Norm the gradient"""
+    if rescale is None:
+        return
+    grad_norm = None
+    for p in model.parameters():
+        if p.grad is None:
+            continue
+        if grad_norm is None:
+            grad_norm = (p.grad.data ** 2).sum()
+        else:
+            grad_norm += (p.grad.data ** 2).sum()
+    grad_norm = np.sqrt(grad_norm)
+    scaling_num = rescale
+    scaling_den = max([rescale, grad_norm])
+    scaling = float(scaling_num) / scaling_den
+
+    for p in model.parameters():
+        if p.grad is None:
+            continue
+        p.grad.data = scaling * p.grad.data
 
 
 class Model(nn.Module):
@@ -272,9 +305,9 @@ class Model(nn.Module):
         self.linp = nn.Embedding(self.n_chars, self.n_hid)
         # n_in is the number of chars, n_out is 3 (pen, X, y)
         self.lproj = GLinear(self.n_out, self.n_hid)
-        self.att_l1 = GaussianAttention(self.n_hid, self.n_hid, self.n_hid,
-                                        self.n_att_components,
-                                        self.minibatch_size)
+        self.att_l1 = GaussianAttentionCell(self.n_hid, self.n_hid, self.n_hid,
+                                            self.n_att_components,
+                                            self.minibatch_size)
         self.proj_to_l2 = GGRUFork(self.n_hid, self.n_hid)
         self.proj_to_l3 = GGRUFork(self.n_hid, self.n_hid)
         self.att_to_l2 = GGRUFork(self.n_hid, self.n_hid)
@@ -340,20 +373,28 @@ class Model(nn.Module):
         # inits[2] = dec_gru1_init
         # inits[3] = dec_gru2_init
 
-        att_h, att_k, att_w = self.att_l1(l1_o, lproj_o, att_gru_init, att_k_init)
-        proj_tm1 = lproj_o[0]
-        h2_tm1 = dec_gru1_init
-        h3_tm1 = dec_gru2_init
-
         hiddens = [Variable(th.zeros(ts, minibatch_size, self.n_hid)) for i in range(3)]
+        att_w = Variable(th.zeros(ts, minibatch_size, self.n_hid))
+        att_k = Variable(th.zeros(ts, minibatch_size, self.n_att_components))
         if use_cuda:
             hiddens = [h.cuda() for h in hiddens]
+            att_w = att_w.cuda()
+            att_k = att_k.cuda()
+        att_inits = self.att_l1.create_inits()
+        att_gru_h_tm1 = Variable(att_inits[0])
+        att_k_tm1 = Variable(att_inits[1])
 
         for i in range(ts):
+            #proj_tm1 = lproj_o[0]
+            h2_tm1 = dec_gru1_init
+            h3_tm1 = dec_gru2_init
+
             proj_t = lproj_o[i]
-            h1_t = att_h[i]
-            w_t = att_w[i]
-            k_t = att_k[i]
+            att_h_t, att_k_t, att_w_t = self.att_l1(l1_o, proj_t, att_gru_h_tm1, att_k_tm1)
+
+            h1_t = att_h_t
+            w_t = att_w_t
+            k_t = att_k_t
 
             inp_f_l2 = self.proj_to_l2(proj_t)
             inp_f_l3 = self.proj_to_l3(proj_t)
@@ -370,6 +411,8 @@ class Model(nn.Module):
 
             h3_t = self.l3(inp_f_l3 + att_f_l3 + l1_f_l3 + l2_f_l3, h3_tm1)
 
+            att_gru_h_tm1 = att_h_t
+            att_k_tm1 = att_k_t
             h2_tm1 = h2_t
             h3_tm1 = h3_t
 
@@ -377,6 +420,9 @@ class Model(nn.Module):
             hiddens[0][i] = hiddens[0][i] + self.att_l1.cell._slice(h1_t, 0)
             hiddens[1][i] = hiddens[1][i] + self.l2._slice(h2_t, 0)
             hiddens[2][i] = hiddens[2][i] + self.l3._slice(h3_t, 0)
+            att_w[i] = att_w[i] + att_w_t
+            att_k[i] = att_k[i] + att_k_t
+
         output = self.loutp1(hiddens[0]) + self.loutp2(hiddens[1]) + self.loutp3(hiddens[2])
         poutput = self.poutp(output)
         mu, sigma, corr, coeff, binary = self._slice_outs(poutput)
@@ -390,9 +436,13 @@ class Model(nn.Module):
              l3_h = l3_h.cuda()
         return self.att_l1.create_inits() + [l2_h, l3_h]
 
-# https://github.com/pytorch/pytorch/issues/2591
+# from A d B
+# https://github.com/adbrebs/handwriting/blob/master/model.py
 def logsumexp(inputs, dim=None):
-    return (inputs - F.log_softmax(inputs)).sum(dim=dim)
+    max_i = inputs.max(dim=dim)[0]
+    z = th.log(th.sum(th.exp(inputs - max_i.expand_as(inputs)), dim=dim)) + max_i
+    return z
+
 
 # https://discuss.pytorch.org/t/build-your-own-loss-function-in-pytorch/235/18
 class BernoulliAndBivariateGMM(nn.Module):
@@ -419,24 +469,26 @@ class BernoulliAndBivariateGMM(nn.Module):
         normalizer = 1. / (2. * 3.14159 * sigma1 * sigma2 * th.sqrt(1. - corr ** 2))
         Z1 = ((t1 - mu1) / sigma1) ** 2
         Z2 = ((t2 - mu2) / sigma2) ** 2
-        zz = (t1 - mu1) / sigma1 * (t2 - mu2) / sigma2
-        # expansion of Z12
-        pp1 = 2 * corr * t1 * t2
-        pp2 = -2 * corr * mu1 * t2
-        pp3 = -2 * corr * mu2 * t1
-        pp4 = 2 * corr * mu1 * mu2
-        denom = 2 * (1. - corr ** 2) / (sigma1 * sigma2)
+        #Z12 = 2 * corr * (t1 - mu1) / sigma1 * (t2 - mu2) / sigma2
+        #Z12 = Z12 * 1. / (2. * (1. - corr ** 2))
+
+        # expansion of Z12?
+        pp1 = 2. * corr * t1 * t2
+        pp2 = -2. * corr * mu1 * t2
+        pp3 = -2. * corr * t1 * mu2
+        pp4 = 2. * corr * mu1 * mu2
+        denom = 2. * (1. - corr ** 2) / (sigma1 * sigma2)
         Z12 = pp1 * denom + pp2 * denom + pp3 * denom + pp4 * denom
 
         Z = -Z1 - Z2 + Z12
         log_gprob = th.log(normalizer) + Z
+        #print("pre_log_gprob: {}:{}".format(log_gprob.cpu().min().data[0], log_gprob.cpu().max().data[0]))
         fcoeff = coeff + self.epsilon
         fcoeff = fcoeff / fcoeff.sum(dim=2).expand_as(coeff)
-        # a bit silly but it works... maybe a way to do the smoothing in logspace directly
-        gprob = th.exp(log_gprob)
-        gprob = gprob + self.epsilon
-        gprob = gprob / gprob.sum(dim=2).expand_as(gprob)
-        log_gprob = th.log(gprob)
+        fgprob = th.exp(log_gprob) + self.epsilon
+        fgprob = fgprob / fgprob.sum(dim=2).expand_as(fgprob)
+        log_gprob = th.log(fgprob)
+        #print("post_log_gprob: {}:{}".format(log_gprob.cpu().min().data[0], log_gprob.cpu().max().data[0]))
 
         """
         # original
@@ -448,60 +500,25 @@ class BernoulliAndBivariateGMM(nn.Module):
         """
         # Thanks to DWF https://gist.github.com/dwf/b2e1d8d575cb9e7365f302c90d909893
         a, t = binary, t0
-        c_b = th.sum(t * F.softplus(-a) + (1. - t) * F.softplus(a), dim=2)
-        nll1 = logsumexp(th.log(fcoeff) + log_gprob, dim=2)[:, :, 0]
-        nll2 = c_b
-        nll = nll1 - nll2
+        c_b = -1. * th.sum(t * F.softplus(-a) + (1. - t) * F.softplus(a), dim=2)
+
+        """
+        # alternate version from BCE_with_logits
+        # from F.binary_cross_entropy_with_logits
+        i = binary
+        t = t0
+        max_val = (-i).clamp(min=0)
+        nc_b = i - i * t + max_val + ((-max_val).exp() + (-i - max_val).exp()).log()
+        """
+
+        ll1 = logsumexp(th.log(fcoeff) + log_gprob, dim=2)[:, :, 0]
+        ll2 = c_b
+        nll = -ll1 - ll2
         return nll
 
 model = Model()
-
 optimizer = th.optim.Adam(model.parameters(), lr=learning_rate)
 loss_function = BernoulliAndBivariateGMM()
-
-"""
-# test the model
-import time
-cinp_mb = c_mb.argmax(axis=-1).astype("int64")
-cinp_mb_v = Variable(th.LongTensor(cinp_mb))
-inp_mb = mb.astype(floatX)
-cuts = int(len(inp_mb) / float(cut_len)) + 1
-# if it is exact, need 1 less
-if (len(inp_mb) % cut_len) == 0:
-    cuts = cuts - 1
-
-use_cuda = True
-if use_cuda:
-    model = model.cuda()
-    cinp_mb_v = cinp_mb_v.cuda()
-
-for e in range(10):
-    start = time.time()
-    for cut_i in range(cuts):
-        inits = [Variable(i) for i in model.create_inits()]
-        inp_mb_v = Variable(th.FloatTensor(inp_mb[cut_i * cut_len:(cut_i + 1) * cut_len]))
-
-        if use_cuda:
-            inits = [i.cuda() for i in inits]
-            inp_mb_v = inp_mb_v.cuda()
-
-        o = model(cinp_mb_v, inp_mb_v, inits)
-        l_full = loss_function(inp_mb_v,o[0], o[1], o[2], o[3], o[4])
-        # sum / mask once adding mask
-        l = l_full.mean()
-        print("TBPTT step {}".format(cut_i))
-        print("loss {}".format(l))
-        print("did loss")
-        l.backward()
-        print("did backward")
-        clip_gradient(model, 10.)
-        print("did clip")
-        optimizer.step()
-        print("did step")
-    end = time.time()
-    print("total time {}".format(end - start))
-from IPython import embed; embed(); raise ValueError()
-"""
 
 if use_cuda:
     model = model.cuda()
@@ -555,25 +572,17 @@ def train_loop(itr, extra):
         l_full = loss_function(target, mu, sigma, corr, coeff, binary)
         # sum / mask once adding mask
         l = l_full.mean()
-        #l.backward()
-        # ???? why retain_variables... TBPTT?
         l.backward(retain_variables=False)
         optimizer.step()
-        clip_gradient(model, 10.)
+        #clip_gradient(model, 10.)
+        norm_gradient(model, 10.)
         total_count += len(inp_mb_sub)
         total_loss = total_loss + l.cpu().data[0] * len(inp_mb_sub)
 
-        # setup next inits for TBPTT
-        #att_k_init = Variable(att_k[-1])
-        #att_gru_init = Variable(hiddens[0][-1])
-        #dec_gru1_init = Variable(hiddens[1][-1])
-        #dec_gru2_init = Variable(hiddens[2][-1])
         att_k_init = Variable(att_k[-1].cpu().data)
         att_gru_init = Variable(hiddens[0][-1].cpu().data)
         dec_gru1_init = Variable(hiddens[1][-1].cpu().data)
         dec_gru2_init = Variable(hiddens[2][-1].cpu().data)
-        #print("Part:", total_loss / float(total_count))
-    #print("Final:", total_loss / float(total_count))
     return [total_loss / float(total_count)]
 
 def valid_loop(itr, extra):
@@ -617,26 +626,19 @@ def valid_loop(itr, extra):
         l_full = loss_function(inp_mb_v, mu, sigma, corr, coeff, binary)
         # sum / mask once adding mask
         l = l_full.mean()
-        #l.backward()
-        # ???? why retain_variables... TBPTT?
         total_count += len(inp_mb_sub)
         total_loss = total_loss + l.cpu().data[0] * len(inp_mb_sub)
 
-        # setup next inits for TBPTT
-        #att_k_init = Variable(att_k[-1])
-        #att_gru_init = Variable(hiddens[0][-1])
-        #dec_gru1_init = Variable(hiddens[1][-1])
-        #dec_gru2_init = Variable(hiddens[2][-1])
-        att_k_init = att_k[-1]
-        att_gru_init = hiddens[0][-1]
-        dec_gru1_init = hiddens[1][-1]
-        dec_gru2_init = hiddens[2][-1]
-        #print("Part:", total_loss / float(total_count))
-    #print("Final:", total_loss / float(total_count))
+        att_k_init = Variable(att_k[-1].cpu().data)
+        att_gru_init = Variable(hiddens[0][-1].cpu().data)
+        dec_gru1_init = Variable(hiddens[1][-1].cpu().data)
+        dec_gru2_init = Variable(hiddens[2][-1].cpu().data)
     return [total_loss / float(total_count)]
 
 
 checkpoint_dict, model, optimizer = create_checkpoint_dict(model, optimizer)
+print(torch_summarize(model))
+
 
 TL = TrainingLoop(train_loop, train_itr,
                   valid_loop, valid_itr,
